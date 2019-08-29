008e1d4b6902d3c0cff9148b14557a6b240f9013
李小朋 <xiaopeng.li@niu.com>
2019-08-29 20:13:30 +0800

    修改线性渐变文字

--- view/src/main/java/com/view/LinearGradientTextView.java
+++ view/src/main/java/com/view/LinearGradientTextView.java
@@ -51,13 +51,24 @@ public class LinearGradientTextView extends android.support.v7.widget.AppCompatT
      * @param top2Bottom true
      */
     public void verticalGradient(final int startColor, final int endColor, final boolean top2Bottom) {
+        verticalGradient(startColor, endColor, top2Bottom, TileMode.CLAMP);
+    }
+
+    /**
+     * 竖直渐变
+     *
+     * @param startColor
+     * @param endColor
+     * @param top2Bottom true
+     */
+    public void verticalGradient(final int startColor, final int endColor, final boolean top2Bottom, final TileMode mode) {
         post(new Runnable() {
             @Override
             public void run() {
-                if (top2Bottom){
-                    linearGradient = new LinearGradient(0, 0, 0, viewHeight, startColor, endColor, TileMode.CLAMP);
-                }else{
-                    linearGradient = new LinearGradient(viewHeight, 0, 0, 0, startColor, endColor, TileMode.CLAMP);
+                if (top2Bottom) {
+                    linearGradient = new LinearGradient(0, 0, 0, viewHeight, startColor, endColor, mode);
+                } else {
+                    linearGradient = new LinearGradient(viewHeight, 0, 0, 0, startColor, endColor, mode);
                 }
                 paint.setShader(linearGradient);
                 postInvalidate();
@@ -73,21 +84,32 @@ public class LinearGradientTextView extends android.support.v7.widget.AppCompatT
      * @param endColor
      * @param left2Right true:左到右渐变 false:右到左渐变
      */
-    public void horizontalGradient(final int startColor, final int endColor,final boolean left2Right) {
-       post(new Runnable() {
-           @Override
-           public void run() {
-               Log.e(TAG, "horizontalGradient: "+getWidth());
-               Log.e(TAG, "horizontalGradient: "+viewWidth);
-               if (left2Right) {
-                   linearGradient = new LinearGradient(0, 0, viewWidth, 0, startColor, endColor, TileMode.CLAMP);
-               } else {
-                   linearGradient = new LinearGradient(viewWidth, 0, 0, 0, startColor, endColor, TileMode.CLAMP);
-               }
-               paint.setShader(linearGradient);
-               postInvalidate();
-           }
-       });
+    public void horizontalGradient(final int startColor, final int endColor, final boolean left2Right) {
+        horizontalGradient(startColor, endColor, left2Right, TileMode.CLAMP);
+    }
+
+    /**
+     * 水平渐变
+     *
+     * @param startColor
+     * @param endColor
+     * @param left2Right true:左到右渐变 false:右到左渐变
+     */
+    public void horizontalGradient(final int startColor, final int endColor, final boolean left2Right, final TileMode mode) {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, "horizontalGradient: " + getWidth());
+                Log.e(TAG, "horizontalGradient: " + viewWidth);
+                if (left2Right) {
+                    linearGradient = new LinearGradient(0, 0, viewWidth, 0, startColor, endColor, mode);
+                } else {
+                    linearGradient = new LinearGradient(viewWidth, 0, 0, 0, startColor, endColor, mode);
+                }
+                paint.setShader(linearGradient);
+                postInvalidate();
+            }
+        });
     }
 
     /**
@@ -101,7 +123,21 @@ public class LinearGradientTextView extends android.support.v7.widget.AppCompatT
      * @param endColor
      */
     public void setGradientColor(int startX, int startY, int endX, int endY, int startColor, int endColor) {
-        linearGradient = new LinearGradient(startX, startY, endX, endY, startColor, endColor, TileMode.CLAMP);
+        setGradientColor(startX, startY, endX, endY, startColor, endColor, TileMode.CLAMP);
+    }
+
+    /**
+     * 为了支持多方向的渐变，参数同{@link LinearGradient#LinearGradient(float, float, float, float, int, int, TileMode)}
+     *
+     * @param startX
+     * @param startY
+     * @param endX
+     * @param endY
+     * @param startColor
+     * @param endColor
+     */
+    public void setGradientColor(int startX, int startY, int endX, int endY, int startColor, int endColor, TileMode mode) {
+        linearGradient = new LinearGradient(startX, startY, endX, endY, startColor, endColor, mode);
         paint.setShader(linearGradient);
         postInvalidate();
     }
@@ -115,7 +151,7 @@ public class LinearGradientTextView extends android.support.v7.widget.AppCompatT
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.e(TAG, "onDraw: "+viewWidth);
+        Log.e(TAG, "onDraw: " + viewWidth);
         super.onDraw(canvas);
     }
 }
