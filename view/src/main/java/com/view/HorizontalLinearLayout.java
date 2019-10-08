ce20ea59d03f91a44394122f0ad2e3623061cecc
lzp <448063828@qq.com>
2019-10-09 00:01:07 +0800

    修改readme

--- view/src/main/java/com/view/HorizontalLinearLayout.java
+++ view/src/main/java/com/view/HorizontalLinearLayout.java
@@ -1,9 +1,7 @@
 package com.view;
 
 import android.content.Context;
-import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
