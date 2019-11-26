3e4910cc02fcd4921eb6d74a256e832e718aa66b
李小朋 <xiaopeng.li@niu.com>
2019-11-26 15:44:57 +0800

    弹性列表兼容垂直方向

--- view/src/main/java/com/view/DampRecyclerView.java
+++ view/src/main/java/com/view/DampRecyclerView.java
@@ -27,13 +27,20 @@ public class DampRecyclerView extends RecyclerView {
     }
 
     private float downX = 0;
+    private float downY = 0;
     private float damp = 0.5f;
+    private int orientation = LinearLayoutManager.VERTICAL;
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent e) {
         int action = e.getAction();
+        LayoutManager layoutManager = getLayoutManager();
+        if (layoutManager instanceof LinearLayoutManager){
+            orientation = ((LinearLayoutManager) layoutManager).getOrientation();
+        }
         if (action == MotionEvent.ACTION_DOWN) {
             downX = e.getX();
+            downY = e.getY();
         }
         return super.onInterceptTouchEvent(e);
     }
@@ -46,23 +53,37 @@ public class DampRecyclerView extends RecyclerView {
                 downX = e.getX();
                 break;
             case MotionEvent.ACTION_MOVE:
-                float nowX = e.getX();
-                float dx = nowX - downX;
-                downX = nowX;
-
-                if (dx > 0 && getFirstItem() == 0) {//在顶部
-                    offsetLeftAndRight((int) (dx * damp));
-                } else {
-                    if (getAdapter() != null) {
-                        if (getLastPosition() == getAdapter().getItemCount() - 1) {//在末尾
-                            offsetLeftAndRight((int) (dx * damp));
+                if (orientation==LinearLayoutManager.HORIZONTAL){
+                    float nowX = e.getX();
+                    float dx = nowX - downX;
+                    downX = nowX;
+                    if (dx > 0 && getFirstItem() == 0) {//在顶部
+                        offsetLeftAndRight((int) (dx * damp));
+                    } else {
+                        if (getAdapter() != null) {
+                            if (getLastPosition() == getAdapter().getItemCount() - 1) {//在末尾
+                                offsetLeftAndRight((int) (dx * damp));
+                            }
+                        }
+                    }
+                }else{
+                    float nowY = e.getY();
+                    float dy = nowY - downY;
+                    downY = dy;
+                    if (dy > 0 && getFirstItem() == 0) {//在顶部
+                        offsetLeftAndRight((int) (dy * damp));
+                    } else {
+                        if (getAdapter() != null) {
+                            if (getLastPosition() == getAdapter().getItemCount() - 1) {//在末尾
+                                offsetLeftAndRight((int) (dy * damp));
+                            }
                         }
                     }
                 }
+
                 break;
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
-
                 doDamp();
                 break;
         }
@@ -90,7 +111,12 @@ public class DampRecyclerView extends RecyclerView {
     private int viewOffset = 0;
 
     private void doDamp() {
-        viewOffset = getLeft();
+        if (orientation==LinearLayoutManager.HORIZONTAL){
+            viewOffset = getLeft();
+        }else{
+            viewOffset = getTop();
+        }
+
         ValueAnimator animator = ValueAnimator.ofInt(viewOffset, 0);
         animator.setDuration(400);
         animator.setInterpolator(new DecelerateInterpolator(2));
@@ -98,9 +124,13 @@ public class DampRecyclerView extends RecyclerView {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 int animatedValue = (int) animation.getAnimatedValue();
-                int dx = animatedValue - viewOffset;
+                int dv = animatedValue - viewOffset;
                 viewOffset = animatedValue;
-                offsetLeftAndRight(dx);
+                if (orientation== LinearLayoutManager.HORIZONTAL){
+                    offsetLeftAndRight(dv);
+                }else{
+                    offsetTopAndBottom(dv);
+                }
             }
         });
         animator.start();
