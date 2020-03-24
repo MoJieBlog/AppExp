8ecdd9469212d548015fd51ecd4409d8dfc54e48
李小朋 <xiaopeng.li@niu.com>
2020-03-24 09:56:04 +0800

    全部组件化完成

---
+++ systemopt/src/main/java/com/lzp/systemopt/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.lzp.systemopt
+
+import android.os.Bundle
+import com.base.compat.BaseActivity
+
+/**
+ * @describe:
+ * @Author: lixiaopeng
+ * @Date: 2020/3/23
+ */
+class MainActivity:BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.systemopt_main_activity)
+    }
+}
---
+++ utilsexample/src/main/java/com/lzp/utilsexample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.lzp.utilsexample
+
+import android.os.Bundle
+import com.base.compat.BaseActivity
+
+/**
+ * @describe:
+ * @Author: lixiaopeng
+ * @Date: 2020/3/23
+ */
+class MainActivity:BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.utils_example_main_activity)
+    }
+}
