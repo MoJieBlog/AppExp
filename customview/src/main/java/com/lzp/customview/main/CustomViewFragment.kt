fa9bd9a73669907958c5e43e47e964aac673de2d
李小朋 <xiaopeng.li@niu.com>
2020-03-27 11:01:28 +0800

    重构基本完成，开始整理自定义View

--- customview/src/main/java/com/lzp/customview/main/CustomViewFragment.kt
+++ customview/src/main/java/com/lzp/customview/main/CustomViewFragment.kt
@@ -6,15 +6,55 @@ import android.view.View
 import android.view.ViewGroup
 import com.base.BaseFragment
 import com.lzp.customview.R
+import kotlinx.android.synthetic.main.customview_fragment.*
 
 /**
  * @describe:
  * @Author: lixiaopeng
  * @Date: 2020/3/24
  */
-class CustomViewFragment: BaseFragment(){
+class CustomViewFragment: BaseFragment(), View.OnClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.customview_fragment,null)
+    }
+
+    override fun setListener() {
+        super.setListener()
+        fiveToggleBtn.setOnClickListener(this)
+        dampRv.setOnClickListener(this)
+        dampVp.setOnClickListener(this)
+        indicator.setOnClickListener(this)
+        dashBoard.setOnClickListener(this)
+        halfCircle.setOnClickListener(this)
+        linearGradientTv.setOnClickListener(this)
+        roundRl.setOnClickListener(this)
+    }
+
+    override fun clearListener() {
+        super.clearListener()
+        fiveToggleBtn.setOnClickListener(null)
+        dampRv.setOnClickListener(null)
+        dampVp.setOnClickListener(null)
+        indicator.setOnClickListener(null)
+        dashBoard.setOnClickListener(null)
+        halfCircle.setOnClickListener(null)
+        linearGradientTv.setOnClickListener(null)
+        roundRl.setOnClickListener(null)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.fiveToggleBtn->{
+
+            }
+            R.id.dampRv->{}
+            R.id.dampVp->{}
+            R.id.indicator->{}
+            R.id.dashBoard->{}
+            R.id.halfCircle->{}
+            R.id.linearGradientTv->{}
+            R.id.roundRl->{}
+        }
     }
 
 }
