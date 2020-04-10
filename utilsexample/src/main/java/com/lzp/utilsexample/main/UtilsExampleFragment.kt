036d450b45e29b961988054b520f6ce2f7fe32c1
李小朋 <xiaopeng.li@niu.com>
2020-04-10 16:03:29 +0800

    修改多选项弹窗

--- utilsexample/src/main/java/com/lzp/utilsexample/main/UtilsExampleFragment.kt
+++ utilsexample/src/main/java/com/lzp/utilsexample/main/UtilsExampleFragment.kt
@@ -1,11 +1,9 @@
 package com.lzp.utilsexample.main
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import com.base.BaseFragment
 import com.dialog.MultipleDialog
